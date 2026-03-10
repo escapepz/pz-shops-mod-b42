1a9af0bdb0c68517893b34437afbabe923e3e5ee
hashtafak <jamestran201@gmail.com>
2026-03-10 20:08:58 +0700

    add support for 42.15.0

--- junction.bat
+++ junction.bat
@@ -2,6 +2,6 @@ rd "C:\ZomboidClient1\mods\Shops"
 rd "C:\ZomboidClient2\mods\Shops"
 rd "C:\Users\PC\Zomboid\mods\Shops"
 
-mklink /J "C:\ZomboidClient1\mods\Shops" "D:\DATA\2025\ProjectZ-improve\PZ Mods\project-zomboid-studio\Shopsb42.worktrees\original\Shops"
+mklink /J "C:\ZomboidClient1\mods\nshopsb42" "D:\DATA\2025\ProjectZ-improve\PZ Mods\2026\Shopsb42.worktrees\original\nshopsb42"
 mklink /J "C:\ZomboidClient2\mods\Shops" "D:\DATA\2025\ProjectZ-improve\PZ Mods\project-zomboid-studio\Shopsb42.worktrees\original\Shops"
 mklink /J "C:\Users\PC\Zomboid\mods\Shops" "D:\DATA\2025\ProjectZ-improve\PZ Mods\project-zomboid-studio\Shopsb42.worktrees\original\Shops"
